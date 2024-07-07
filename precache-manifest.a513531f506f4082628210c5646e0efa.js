779ad37549c71cbdb265b672ed0f1b0dd421e774
Roderik <reclamegraag@gmail.com>
2024-07-07 16:20:35 +0200

    Updates

---
+++ precache-manifest.a513531f506f4082628210c5646e0efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667fd950627d3f4e38c563047bf3cdcb",
+    "url": "/vakantie-paklijst/index.html"
+  },
+  {
+    "revision": "b16bfd40713409da010a",
+    "url": "/vakantie-paklijst/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/vakantie-paklijst/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/vakantie-paklijst/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "b16bfd40713409da010a",
+    "url": "/vakantie-paklijst/static/js/main.b9cc5089.chunk.js"
+  },
+  {
+    "revision": "786c8974fcf27693329f",
+    "url": "/vakantie-paklijst/static/js/runtime~main.1ad133df.js"
+  }
+]);
